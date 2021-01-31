78ab508427e344b45d0a57c1cb2c4d422c86b91e
lukabajic <lukabajic.dev@gmail.com>
2021-01-31 17:28:09 +0100

    Add reusable compoenent to render fields

---
+++ components/FormFields.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import Field from "./Field";
+
+const FormFields = ({ form, onChange, onBlur, style = {} }) => (
+  <View style={[styles.wrapper, style]}>
+    {Object.keys(form.fields).map((field, index) => (
+      <Field
+        key={field}
+        label={form.fields[field].label}
+        type={form.fields[field].type}
+        placeholder={form.fields[field].placeholder}
+        meta={form.fields[field].meta}
+        onChange={(value) => onChange(field, value)}
+        onBlur={() => onBlur(field, form.values[field])}
+        value={form.values[field]}
+        autoFocus={form.fields[field].autoFocus}
+        spacing={Object.keys(form.fields).length > index}
+      />
+    ))}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    alignItems: "center",
+  },
+});
+
+export default FormFields;
