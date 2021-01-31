37561487d9ef078da7a715b3ddb98ae0adbe55c6
lukabajic <lukabajic.dev@gmail.com>
2021-01-31 17:28:45 +0100

    Cleanup

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import NoTokenStack from "./navigator/NoTokenNavigator";
-import SignUp from "./screens/SignUp";
 
 export default function App({ token }) {
   if (!token) return <NoTokenStack />;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
