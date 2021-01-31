8abbfa7f7378348d15706795db8ddfb61170ed94
lukabajic <lukabajic.dev@gmail.com>
2021-01-31 14:57:36 +0100

    Outsource screen options

---
+++ utils/screenOptions.js
@@ -0,0 +1,15 @@
+import Colors from "../utils/colors";
+
+export default {
+  headerTintColor: Colors.primary,
+  cardStyle: { backgroundColor: Colors.white },
+  headerBackTitleVisible: false,
+  headerTitleStyle: {
+    color: Colors.black,
+    fontSize: 17,
+  },
+  headerStyle: {
+    backgroundColor: Colors.white,
+    shadowOffset: { height: 0 },
+  },
+};
