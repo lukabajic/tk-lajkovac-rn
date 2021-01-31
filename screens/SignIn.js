6a22c8448f31d2dc3dfefa239dbc81a37bdf4013
lukabajic <lukabajic.dev@gmail.com>
2021-01-31 14:34:35 +0100

    Add boilerplate screens

---
+++ screens/SignIn.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SignIn = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>SignIn</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export default SignIn;
---
+++ screens/SignUp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SignUp = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>SignUp</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export default SignUp;
