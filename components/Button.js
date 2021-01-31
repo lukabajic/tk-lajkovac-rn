ea02d4ae3c7df148b85819198a7bea1a0bd6f12c
lukabajic <lukabajic.dev@gmail.com>
2021-01-31 14:58:52 +0100

    Fix

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet, Text } from "react-native";
 
-import Colors from "../../utils/colors";
+import Colors from "../utils/colors";
 
 // android styles, ionic-like
 
