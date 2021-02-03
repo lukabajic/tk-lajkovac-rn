c109ed67ea6bd7df7c5d2c3c42699b2c4b5a9148
lukabajic <lukabajic.dev@gmail.com>
2021-02-03 15:25:27 +0100

    Add colors back

---
+++ utils/colors.js
@@ -0,0 +1,14 @@
+export default {
+  /* grayscale */
+  white: "#f8f9fa",
+  lightGray: "#e9ecef",
+  gray: "#adb5bd",
+  darkGray: "#343a40",
+  black: "#212529",
+  /* colors */
+  primary: "#2a9d8f",
+  yellow: "#ffcc00",
+  orange: "#ff9500",
+  red: "#ff3b30",
+  blue: "#06c",
+};
