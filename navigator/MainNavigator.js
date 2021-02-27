88d9879f952b017693a0436fe3daf7958be32765
lukabajic <lukabajic.dev@gmail.com>
2021-02-27 09:58:41 +0100

    Fetch user if token persisted

--- navigator/MainNavigator.js
+++ navigator/MainNavigator.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import NoTokenStack from "./NoTokenStack";
 import NoInfoScreen from "../screens/user/NoInfoScreen";
 import PleaseVerifyScreen from "../screens/user/PleaseVerifyScreen";
 import MainDrawer from "./MainDrawer";
+import { fetchCurUser } from "../store/actions";
+import Loader from "../components/Loader";
 
-const MainNavigator = ({ token, user }) => {
+const MainNavigator = ({ token, user, loading, fetchCurUser }) => {
   if (!token) return <NoTokenStack />;
 
-  const { displayName, phone } = user.data;
+  if (loading) return <Loader />;
+
+  useEffect(() => {
+    if (token && !user) fetchCurUser(token);
+  }, [token, user]);
+
+  const displayName = user?.data?.displayName;
+  const phone = user?.data?.phone;
 
   if (!displayName || !phone) return <NoInfoScreen />;
 
@@ -20,7 +29,11 @@ const MainNavigator = ({ token, user }) => {
   return <MainDrawer />;
 };
 
-export default connect((state) => ({
-  token: state.auth.token,
-  user: state.user.user,
-}))(MainNavigator);
+export default connect(
+  (state) => ({
+    token: state.auth.token,
+    user: state.user.user,
+    loading: state.user.loading,
+  }),
+  { fetchCurUser }
+)(MainNavigator);
