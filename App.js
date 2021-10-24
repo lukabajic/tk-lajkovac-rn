fc6beeffaf28114583e505b5aa8d332cef15cf6d
lukabajic <lukabajic.dev@gmail.com>
2021-10-24 12:48:25 +0200

    Add status bar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import MainNavigator from "./navigator/MainNavigator";
-import { NavigationContainer } from "@react-navigation/native";
+import MainNavigator from './navigator/MainNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
-import redux from "./store";
+import redux from './store';
 
 const store = redux().store;
 const persistor = redux().persistor;
@@ -14,6 +15,7 @@ const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <NavigationContainer>
+        <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
         <MainNavigator />
       </NavigationContainer>
     </PersistGate>
