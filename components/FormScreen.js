fc478e46259372b462c242abf54c37a71f64f1db
lukabajic <lukabajic.dev@gmail.com>
2021-10-24 12:51:30 +0200

    Group stuff on form screens to scroll together

--- components/FormScreen.js
+++ components/FormScreen.js
@@ -1,34 +1,36 @@
-import React from "react";
+import React from 'react';
 import {
   KeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
   StyleSheet,
   View,
-} from "react-native";
+} from 'react-native';
 
-import Colors from "../utils/colors";
-import Alert from "./Alert";
-import { LargeTitle, Headline } from "./Typography";
+import Colors from '../utils/colors';
+import Alert from './Alert';
+import { LargeTitle, Headline } from './Typography';
 
 const FormScreen = ({ children, title, subtitle, bottomContent, error }) => (
   <SafeAreaView style={styles.screen}>
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : null}
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
       keyboardVerticalOffset={40}
       style={styles.wrapper}
     >
       <ScrollView bounces={false} contentContainerStyle={styles.form}>
-        {(title || subtitle) && (
-          <View style={styles.header}>
-            {title && <LargeTitle style={styles.title}>{title}</LargeTitle>}
-            {subtitle && <Headline style={styles.title}>{subtitle}</Headline>}
-          </View>
-        )}
-        {error && <Alert message={error} type="danger" />}
-        {children}
+        <View style={styles.formAndButton}>
+          {(title || subtitle) && (
+            <View style={styles.header}>
+              {title && <LargeTitle style={styles.title}>{title}</LargeTitle>}
+              {subtitle && <Headline style={styles.title}>{subtitle}</Headline>}
+            </View>
+          )}
+          {error && <Alert message={error} type="danger" />}
+          {children}
+        </View>
+        {bottomContent}
       </ScrollView>
-      {bottomContent}
     </KeyboardAvoidingView>
   </SafeAreaView>
 );
@@ -43,12 +45,18 @@ const styles = StyleSheet.create({
   },
   form: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  formAndButton: {
+    width: '100%',
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.black,
   },
   header: {
